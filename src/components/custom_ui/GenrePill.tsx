013162c981f3b04076db53cd5d641e6eface09bd
xjeoo <fnaffoxy1234@gmail.com>
2025-10-03 18:58:25 +0300

    style(home_page): changed carousel font sizes

--- src/components/custom_ui/GenrePill.tsx
+++ src/components/custom_ui/GenrePill.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GenrePill = ({ text }: { text: string }) => {
   return (
-    <span className="text-white outline-1 outline-white px-1.5 py-0.5 rounded-full">
+    <span className="text-white text-[0.9em] sm:text-[1em] outline-1 outline-white px-1.5 py-0.5 rounded-full">
       {text}
     </span>
   );
