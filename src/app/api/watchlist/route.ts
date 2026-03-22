7170a38944475fd5969d9eb672c542c202a77b84
xjeoo <fnaffoxy1234@gmail.com>
2026-03-22 23:28:17 +0200

    feat: added GET and PATCH methods to watchlist routes
    get - for getting watchlist items for current user; patch - for updating watch status of specific item for current user

--- src/app/api/watchlist/route.ts
+++ src/app/api/watchlist/route.ts
@@ -1,9 +1,24 @@
 import { decrypt } from "@/lib/auth/jwt";
 import { getToken } from "@/lib/auth/sessionUtils";
-import { deleteIfExistsFromWatchlist, existsInWatchlist, savetoWatchlist } from "@/lib/user/watchlist";
+import { deleteIfExistsFromWatchlist, existsInWatchlist, getUserWatchlist, savetoWatchlist, updateWatchlistItem } from "@/lib/user/watchlist";
 import { NextRequest, NextResponse } from "next/server";
 
 
+export async function GET(request: NextRequest){
+
+  const token = await getToken();
+  if(!token) return NextResponse.json({error: "Token missing"}, {status:401});
+
+  const jwt  = await decrypt(token);
+  if(!jwt) return NextResponse.json({error: "Invalid token"}, {status:403});
+  const userId = jwt.data.userId.toString();
+  if(!userId )return NextResponse.json({error: "User id missing"}, {status:400});
+
+  const watchlist = await getUserWatchlist(userId);
+  if(watchlist === null) return NextResponse.json({error: "Failed fetching watchlist"}, {status:500});
+  return NextResponse.json({watchlist: watchlist}, {status:200});
+
+}
 export async function POST(request : NextRequest){
 
 
@@ -48,4 +63,31 @@ export async function DELETE(request: NextRequest){
 
   return NextResponse.json({message: "Deleted from watchlist"}, {status:200});
 
+}
+
+export async function PATCH(request: NextRequest){
+
+  const token = await getToken();
+  if(!token) return NextResponse.json({error: "Token missing"}, {status:401});
+
+  const jwt  = await decrypt(token);
+  if(!jwt) return NextResponse.json({error: "Invalid token"}, {status:403});
+  const userId = jwt.data.userId.toString();
+
+  const {contentId, type, newStatus} = await request.json();
+
+  if(!contentId || !userId || !newStatus)return NextResponse.json({error: "Item, user id or new status missing"}, {status:400});
+  
+  // CHECK WATCHLISTMODEL TS FOR VALID STATUS VALUES
+  if (newStatus !== "Not watched" && newStatus !== "Currently watching" && newStatus !== "Watched") {
+    console.log("Invalid watch status:", newStatus);
+    return NextResponse.json({ error: "Invalid watch status" }, { status: 400 });
+  }
+  
+  const exists = await existsInWatchlist(contentId, userId, type);
+  if(!exists) return NextResponse.json({error: "Item not in watchlist"}, {status:401});
+  const updated = await updateWatchlistItem(contentId, userId, type, newStatus);
+  if(!updated) return NextResponse.json({error: "Failed updating watchlist item"}, {status:500});
+
+  return NextResponse.json({message: "Updated watchlist item"}, {status:200});
 }
--- src/lib/user/watchlist.ts
+++ src/lib/user/watchlist.ts
@@ -1,10 +1,21 @@
 import Watchlist from "@/models/Watchlist";
 import dbConnect from "../database";
 
+export async function getUserWatchlist(userId: string) {
+  await dbConnect();
+  try {
+    const watchlist = await Watchlist.find({ userId: userId }).sort({ createdAt: -1 });
+    return watchlist;
+  } catch (err) {
+    console.log("Failed to fetch watchlist", err);
+    throw new Error("Database failed fetching watchlist");
+  }
+}
+
 export async function existsInWatchlist(
   contentId: string,
   userId: string | undefined,
-  type: string
+  type: string,
 ) {
   await dbConnect();
 
@@ -53,17 +64,38 @@ export async function savetoWatchlist(
 export async function deleteIfExistsFromWatchlist(
   contentId: string,
   userId: string,
-  type: string
+  type: string,
 ) {
   await dbConnect();
   try {
-    const toDelete = await Watchlist.findOne({contentId, userId, type})
-    if(!toDelete) return true;
+    const toDelete = await Watchlist.findOne({ contentId, userId, type });
+    if (!toDelete) return true;
     await toDelete.deleteOne();
     return true;
-    
   } catch (err) {
     console.log("Failed to delete from watchlist", err);
+    return false;
+  }
+}
+
+export async function updateWatchlistItem(
+  contentId: string,
+  userId: string,
+  type: string,
+  newStatus: string
+){
+  
+  await dbConnect();
+  try {
+    const item = await Watchlist.findOne({ contentId, userId, type });
+    if(!item) return false;
+    item.watchStatus = newStatus;
+    await item.save();
+    return true;
+
+   
+  } catch (err) {
+    console.log("Failed to update watchlist item", err);
     return false;
   }
 }
