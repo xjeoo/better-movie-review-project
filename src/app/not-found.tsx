4e10dff9795ece833bd5e22fcbf04168f96d8602
xjeoo <fnaffoxy1234@gmail.com>
2026-03-26 01:13:21 +0200

    fix: replaced anchor tag with next/link and removed apostrophe

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
+
 const NotFound = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-black px-4">
       <h1 className="text-6xl font-bold text-blue-primary-darker mb-4">404</h1>
-      <p className="text-xl text-white mb-8">Oops, this page doesn't exist</p>
-      <a
+      <p className="text-xl text-white mb-8">
+        Oops, this page doesn&apos;t exist
+      </p>
+      <Link
         href="/"
         className="px-6 py-3 bg-blue-primary-darker text-white rounded-lg font-semibold hover:bg-blue-primary-darker-100 transition-colors"
       >
         Go Back Home
-      </a>
+      </Link>
     </div>
   );
 };
